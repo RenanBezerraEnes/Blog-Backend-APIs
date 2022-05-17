955fb51ffa265940647f16509c59210c34faf4b3
Renan Bezerra <renanbezerraenes@icloud.com>
2022-05-17 19:44:30 +0200

    Cloudinary file, to add files and convert it into a link  and save on the dataBase

---
+++ src/lib/cloudiary.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import createError from "http-errors";
+
+export const cloudinaryUploader = multer({
+	storage: new CloudinaryStorage({
+		cloudinary,
+		params: {
+			folder: "Blog Application/Authors - Avatar",
+		},
+	}),
+	limits: { fileSize: 1 * 1024 * 1024 },
+	fileFilter: (req, file, multerNext) => {
+		if (file.mimetype !== "image/jpeg") {
+			return multerNext(createError(400, "Only images and Gifs are allowed"));
+		} else {
+			multerNext(null, true);
+		}
+	},
+}).single("avatar");
