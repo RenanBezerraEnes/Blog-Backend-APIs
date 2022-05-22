5933f88af7cec6dcf916b5fc3d23925e9eb4fc09
Renan Bezerra <renanbezerraenes@icloud.com>
2022-05-22 17:08:21 +0200

    JWT token added to the endpoints

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -6,6 +6,8 @@ import { checkAuthorMiddleware, checkVdalidationResult } from "./validation.js";
 import query2Mongo from "query-to-mongo";
 import { cloudinaryUploader } from "../../lib/cloudiary.js";
 import { authorWelcome } from "../../lib/sendEmail.js";
+import { JWTAuthMiddleware } from "../../lib/auth/token.js";
+import { adminOnlyMiddleware } from "../../lib/auth/admin.js";
 
 const authorsRouter = express.Router();
 
@@ -13,6 +15,8 @@ authorsRouter.post(
 	"/",
 	checkAuthorMiddleware,
 	checkVdalidationResult,
+	JWTAuthMiddleware,
+	adminOnlyMiddleware,
 	async (req, res, next) => {
 		try {
 			const newAuthor = new AuthorsModel(req.body);
@@ -28,102 +32,126 @@ authorsRouter.post(
 	}
 );
 
-authorsRouter.get("/", async (req, res, next) => {
-	try {
-		const mongoQuery = query2Mongo(req.query);
+authorsRouter.get(
+	"/",
+	JWTAuthMiddleware,
+	adminOnlyMiddleware,
+	async (req, res, next) => {
+		try {
+			const mongoQuery = query2Mongo(req.query);
 
-		const total = await AuthorsModel.countDocuments(mongoQuery.criteria);
+			const total = await AuthorsModel.countDocuments(mongoQuery.criteria);
 
-		if (!mongoQuery.options.skip) mongoQuery.options.skip = 0;
+			if (!mongoQuery.options.skip) mongoQuery.options.skip = 0;
 
-		if (!mongoQuery.options.limit || mongoQuery.options.limit > 10)
-			mongoQuery.options.limit = 20;
+			if (!mongoQuery.options.limit || mongoQuery.options.limit > 10)
+				mongoQuery.options.limit = 20;
 
-		const authors = await AuthorsModel.find(
-			mongoQuery.criteria,
-			mongoQuery.options.fields
-		)
-			.skip(mongoQuery.options.skip)
-			.limit(mongoQuery.options.limit)
-			.sort(mongoQuery.options.sort);
+			const authors = await AuthorsModel.find(
+				mongoQuery.criteria,
+				mongoQuery.options.fields
+			)
+				.skip(mongoQuery.options.skip)
+				.limit(mongoQuery.options.limit)
+				.sort(mongoQuery.options.sort);
 
-		res.send({
-			links: mongoQuery.links(`${process.env.Blogs_API}/blogPosts`, total),
-			total,
-			totalPages: Math.ceil(total / mongoQuery.options.limit),
-			authors,
-		});
-	} catch (error) {
-		next(error);
-	}
-});
-
-// BLOGS
-// Extra Features
-// GET /authors/:id/blogPosts/ => get all the posts for an author with a given ID
-authorsRouter.get("/:authorId/blogPosts", async (req, res) => {
-	try {
-		const blog = await BlogModel.find({ author: req.params.authorId });
-		if (blog) {
-			res.send(blog);
-		} else {
-			next(createError(404`User with id ${req.params.userId} not found`));
+			res.send({
+				links: mongoQuery.links(`${process.env.Blogs_API}/blogPosts`, total),
+				total,
+				totalPages: Math.ceil(total / mongoQuery.options.limit),
+				authors,
+			});
+		} catch (error) {
+			next(error);
 		}
-	} catch (error) {
-		next(error);
 	}
-});
+);
 
-authorsRouter.get("/:authorId", async (req, res) => {
-	try {
-		const author = await AuthorsModel.findById(req.params.authorId);
-		if (author) {
-			res.send(author);
-		} else {
-			next(createError(404`User with id ${req.params.userId} not found`));
+authorsRouter.get(
+	"/:authorId/blogPosts",
+	JWTAuthMiddleware,
+	adminOnlyMiddleware,
+	async (req, res) => {
+		try {
+			const blog = await BlogModel.find({ author: req.params.authorId });
+			if (blog) {
+				res.send(blog);
+			} else {
+				next(createError(404`User with id ${req.params.userId} not found`));
+			}
+		} catch (error) {
+			next(error);
 		}
-	} catch (error) {
-		next(error);
 	}
-});
+);
 
-authorsRouter.put("/:authorId", async (req, res) => {
-	try {
-		const updateAuthor = await AuthorsModel.findByIdAndUpdate(
-			req.params.authorId,
-			req.body,
-			{ new: true }
-		);
-		if (updateAuthor) {
-			res.send(updateAuthor);
-		} else {
-			next(createError(404`User with id ${req.params.userId} not found`));
+authorsRouter.get(
+	"/:authorId",
+	JWTAuthMiddleware,
+	adminOnlyMiddleware,
+	async (req, res) => {
+		try {
+			const author = await AuthorsModel.findById(req.params.authorId);
+			if (author) {
+				res.send(author);
+			} else {
+				next(createError(404`User with id ${req.params.userId} not found`));
+			}
+		} catch (error) {
+			next(error);
 		}
-	} catch (error) {
-		next(error);
 	}
-});
+);
 
-authorsRouter.delete("/:authorId", async (req, res) => {
-	try {
-		const deleteAuthors = await AuthorsModel.findByIdAndDelete(
-			req.params.authorId
-		);
-		if (deleteAuthors) {
-			res.status(204).send();
-		} else {
-			next(createError(404`User with id ${req.params.userId} not found`));
+authorsRouter.put(
+	"/:authorId",
+	JWTAuthMiddleware,
+	adminOnlyMiddleware,
+	async (req, res) => {
+		try {
+			const updateAuthor = await AuthorsModel.findByIdAndUpdate(
+				req.params.authorId,
+				req.body,
+				{ new: true }
+			);
+			if (updateAuthor) {
+				res.send(updateAuthor);
+			} else {
+				next(createError(404`User with id ${req.params.userId} not found`));
+			}
+		} catch (error) {
+			next(error);
 		}
-	} catch (error) {
-		next(error);
 	}
-});
+);
+
+authorsRouter.delete(
+	"/:authorId",
+	JWTAuthMiddleware,
+	adminOnlyMiddleware,
+	async (req, res) => {
+		try {
+			const deleteAuthors = await AuthorsModel.findByIdAndDelete(
+				req.params.authorId
+			);
+			if (deleteAuthors) {
+				res.status(204).send();
+			} else {
+				next(createError(404`User with id ${req.params.userId} not found`));
+			}
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 
 // AUTHORS AVATAR POST
 
 authorsRouter.post(
 	"/:authorId/avatar",
 	cloudinaryUploader,
+	JWTAuthMiddleware,
+	adminOnlyMiddleware,
 	async (req, res, next) => {
 		try {
 			console.log(req.file.path, "LOOK ME");
